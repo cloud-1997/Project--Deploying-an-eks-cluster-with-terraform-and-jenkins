d0a064dd29762c0190f3663bee7be2d598e3246e
cloud-rohit-1997 <160157203+cloud-rohit-1997@users.noreply.github.com>
2024-08-16 22:33:26 +0530

    Updated the  jenkins-install.sh

--- jenkins-server-terra/jenkins-install.sh
+++ jenkins-server-terra/jenkins-install.sh
@@ -23,3 +23,22 @@ sudo systemctl enable jenkins
 
 # Start the Jenkins service
 sudo systemctl start jenkins
+
+# Install git in the machine 
+apt install git -y
+
+# Install teraform in the machine
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
+
+# Install kubectl in the machine
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+
+
+
+
